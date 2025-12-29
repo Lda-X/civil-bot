9e4730ef16e4bf8fa3268e7501be92cb33e3ae8b
Dawn <151519694+Lda-X@users.noreply.github.com>
2025-12-30 01:10:33 +0800

    Update app3.py

--- app3.py
+++ app3.py
@@ -8,14 +8,10 @@ import uuid
 from datetime import datetime
 from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import ZhipuAIEmbeddings               
+from langchain_text_splitters import RecursiveCharacterTextSplitter 
 from langchain_community.embeddings import ZhipuAIEmbeddings
-# from langchain.docstore.document import Document
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import ZhipuAIEmbeddings
-
 from langchain_core.documents import Document
-
 from zhipuai import ZhipuAI
 
 #配置与初始化
@@ -743,4 +739,5 @@ if current_messages and current_messages[-1]["role"] == "user":
 
 
             except Exception as e:
+
                 st.error(f"生成回答出错: {e}")
